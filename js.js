d59ce03cdbd4efb4de98ee34494911661c7ebff2
yigitjon <103301096+yigitjon@users.noreply.github.com>
2022-04-14 13:06:16 +0500

    tegilmasin
    tegmang

---
+++ js.js
@@ -0,0 +1,19 @@
+console.log(document);
+var text = "Darsda o'rgangan 4ta narsa!";
+alert(text);
+confirm(text);
+document.write(text);
+console.log(text);
+alert('siz mening tuzgan saytimni kurmoqchimisz');
+
+confirm("Siz rostan kurmoqchimisiz");
+alert("O'tmang");
+const HappyBirthday = "Saytimni uzi yuq qayerda kurasiz unda?";
+alert(HappyBirthday);
+const button = document.getElementById("button");
+let age = prompt('How old are you?', 11);
+
+alert(`You are ${age} years old!`);
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss );
